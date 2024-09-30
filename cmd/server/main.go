3a050fe7616c8227c4111f1d4148fb1c765ec889
masafumi machida <masafumi-machida1@dip-net.co.jp>
2024-09-30 13:06:47 +0900

    chapter1 code add

---
+++ cmd/server/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+
+	"github.com/dip-masafumi-machida1/letsgo/internal/server"
+)
+
+func main() {
+	srv := server.NewHTTPServer(":8080")
+	log.Fatal(srv.ListenAndServe())
+}
+
+// curl -X POST localhost:8080 -d '{"record": {"value": "TGV0J3MgR28gIzEK"}}'
+// curl -X POST localhost:8080 -d '{"record": {"value": "TGV0J3MgR28gIzIK"}}'
+// curl -X POST localhost:8080 -d '{"record": {"value": "TGV0J3MgR28gIzMK"}}'
